244fa7d312be1ebfedbdaaeb700addf46573cf0f
K-developer <kirk.cumins65@gmail.com>
2019-10-24 20:54:26 +0100

    updates to moving the player

--- solutions/rust/06_text_adventure/kirk_jamie/src/main.rs
+++ solutions/rust/06_text_adventure/kirk_jamie/src/main.rs
@@ -2,17 +2,26 @@ use std::io;
 mod person;
 mod room;
 
-use crate::room::{Room};
+use crate::room::{Room,Door};
 use crate::person::{Person};
 
-fn go(direction: &str ) {
-    match direction.to_lowercase().trim() {
-            "north" => println!("You Went North"),
-            "south" => println!("You Went South"),
-            "west" => println!("You Went West"),
-            "east" => println!("You Went East"),
-            _ => println!("Cannot do that: {}", direction),
-        }
+
+fn go(direction: &str, player: &Person, dungeon: &Vec<Room> ) {
+    // match direction.to_lowercase().trim() {
+    //         "north" => change_room(direction),
+    //         "south" => println!("You Went South"),
+    //         "west" => println!("You Went West"),
+    //         "east" => println!("You Went East"),
+    //         _ => println!("Cannot do that: {}", direction),
+    //     }s
+    let mut rooms = IntoIterator::into_iter(dungeon);
+    let room = rooms.find(|&room| room.name == player.current_room).unwrap();
+    let mut destinations = IntoIterator::into_iter(room.doors);
+    let destination = destinations.find(move |&door_dir| door_dir.direction == direction).unwrap();
+
+    println!("{}",room.doors[0].name);
+    println!("{}", destination.direction);
+
 }
 
 
@@ -22,15 +31,14 @@ fn main() {
         io::stdin().read_line(&mut name)
             .ok()
             .expect("failed to read line");
-    let player = Person::new(name, "North".to_string());
+    let mut player = Person::new(name, "North".to_string());
     println!("Hi {}", player.name);
 
-
     let mut dungeon = Vec::new();
-    dungeon.push(Room::new("North".to_string(), vec!["West", "South"]));
-    dungeon.push(Room::new("West".to_string(), vec!["North"]));
-    dungeon.push(Room::new("East".to_string(), vec!["South"]));
-    dungeon.push(Room::new("South".to_string(), vec!["East", "North"]));
+    dungeon.push(Room::new("North".to_string(), vec![Door::new("West","West"), Door::new("South","South")]));
+    dungeon.push(Room::new("West".to_string(), vec![Door::new("North","North")]));
+    dungeon.push(Room::new("East".to_string(), vec![Door::new("South","South")]));
+    dungeon.push(Room::new("South".to_string(), vec![Door::new("East","East"), Door::new("North","North")]));
     loop {
         let mut command = String::new(); 
         println!("Enter your choice");
@@ -43,7 +51,7 @@ fn main() {
         let object = commands.get(1).unwrap_or(&"");
 
         match action.trim() {
-            "go" => go(object),
+            "go" => go(object,&player, &dungeon),
             "quit" => break,
             _ => println!("Cannot do that: {}", object),
         }
--- solutions/rust/06_text_adventure/kirk_jamie/src/room.rs
+++ solutions/rust/06_text_adventure/kirk_jamie/src/room.rs
@@ -1,13 +1,27 @@
-pub struct Room{
+pub struct Room<'a>{
     pub name: String,    
-    pub doors: Vec<'a&str>,
+    pub doors: Vec<Door<'a>>,
 }
 
-impl Room{
-    pub fn new(name: String, doors: Vec<'a&str>) -> Room {
+impl<'a> Room<'a>{
+    pub fn new(name: String, doors: Vec<Door<'a>>) -> Room <'a>{
         Room {
             name: name,
             doors: doors,
+        }
+    }
+}
+
+pub struct Door<'a>{
+    pub name: &'a str,
+    pub direction: &'a str
+}
+
+impl<'a> Door<'a>{
+    pub fn new(name: &'a str, direction: &'a str) -> Door<'a> {
+        Door {
+            name: name,
+            direction: direction,
         }
     }
 }
